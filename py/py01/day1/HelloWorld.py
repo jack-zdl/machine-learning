044b88a90daa0a9eed310c3db9e99bfe680ac232
zhangdelei <zhangdelei000@gmail.com>
2018-03-14 21:07:49 +0800

    开发外星人入侵python项目

---
+++ py/py01/day1/HelloWorld.py
@@ -0,0 +1,18 @@
+# py注释
+message = "hello world"
+print("hi "+message)
+message = "revise hello world"
+print("hi "+message)
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+favorite = "python "
+print(favorite)
+print(favorite.rstrip())
+print(2**3)
+print(3*0.1)
+names = ["zhang","wu","song"]
+print(names[0])
+print(names[0].title())
+print(names[-1])  # 可以是循环的
